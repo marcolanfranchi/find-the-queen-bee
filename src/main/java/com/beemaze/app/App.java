b4d0f4d652de44c903325ca2d0a02c8ab46cf6f4
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-10-20 18:21:08 -0700

    Maven Project init

---
+++ src/main/java/com/beemaze/app/App.java
@@ -0,0 +1,13 @@
+package com.beemaze.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
