5eea4dc6ce385560b7dbc53f10d5120eda8b0250
marco lanfranchi <mgl11@sfu.ca>
2022-10-26 10:55:04 -0700

    Added size field to Entity

--- src/main/java/Entity/Entity.java
+++ src/main/java/Entity/Entity.java
@@ -6,10 +6,17 @@ abstract public class Entity {
 
     public int worldX, worldY;
     public int speed;
+    public int width = 32;
+    public int height = 32;
     	
     public int spriteCounter = 0;
     public int spriteNum = 1;
 	
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
     public String direction;
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
 }
