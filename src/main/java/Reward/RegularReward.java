58b676829126e5a522291e5ecc8c25b7d8737da6
sdallalz <sdallalz@sfu.ca>
2022-11-01 21:25:29 -0700

    Adding Generator

--- src/main/java/Reward/RegularReward.java
+++ src/main/java/Reward/RegularReward.java
@@ -8,7 +8,7 @@ import java.awt.image.BufferedImage;
 
 import java.awt.*;
 
-// Randomly appears!!
+
 public class RegularReward extends Reward {
     public RegularReward(int value, BufferedImage texture, Point location, GamePanel map) {
         super(value, texture, location, map);
