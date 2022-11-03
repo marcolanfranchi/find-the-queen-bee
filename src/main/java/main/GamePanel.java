e958707af7052242782341303873c89ccc283b75
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-11-03 00:44:35 -0700

    fix: Changed number of enemies

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -39,7 +39,7 @@ public class GamePanel extends JPanel implements Runnable {
 
 	// Entities, Rewards, & Objects
 	public Bee bee = new Bee(this, keyHandler);
-	public Enemy[] enemies = new Enemy[3];
+	public Enemy[] enemies = new Enemy[2];
 	public Reward rewards[] = new Reward[20];
 	public RewardGenerator rewardGenerator = new RewardGenerator(this);
 	public SuperObject objects[] = new SuperObject[20];
