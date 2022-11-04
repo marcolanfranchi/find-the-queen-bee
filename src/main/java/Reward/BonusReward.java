e8d8b3a62a4b58b072270d9d230b89f5c1f5bbb6
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-11-03 17:54:34 -0700

    bonus rewardtime fix

--- src/main/java/Reward/BonusReward.java
+++ src/main/java/Reward/BonusReward.java
@@ -3,6 +3,10 @@ import main.GamePanel;
 
 import java.io.IOException;
 
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.imageio.ImageIO;
 
 import Entity.Bee;
@@ -14,12 +18,16 @@ public class BonusReward extends Reward {
     public static int bonusRewardVal = 50;
     public boolean displayNow;
     public int drawBuffer = 10;
+	public Timer timer;
 
     public BonusReward(GamePanel gp) {
         super(gp);
         this.value = bonusRewardVal;
         this.displayNow = false;
         this.value = bonusRewardVal;
+		switchVisibility();
+		timer.start();
+
         try {
             this.image = ImageIO.read(getClass().getResource("../ui/images/HoneyPot.png"));
         } catch (IOException e) {
@@ -34,14 +42,28 @@ public class BonusReward extends Reward {
         int screenY = worldY - g.bee.worldY + g.bee.screenY;
 
         if (worldX + g.tileSize > g.bee.worldX - g.bee.screenX &&
-                    worldX - g.tileSize < g.bee.worldX + g.bee.screenX &&
-                    worldY + g.tileSize > g.bee.worldY - g.bee.screenY &&
-                    worldY - g.tileSize < g.bee.worldY + g.bee.screenY) {
-                        if (this.collected == false) {
-                            g2.drawImage(image, screenX, screenY, width, height, null);
-                        }
-                    }
+				worldX - g.tileSize < g.bee.worldX + g.bee.screenX &&
+				worldY + g.tileSize > g.bee.worldY - g.bee.screenY &&
+				worldY - g.tileSize < g.bee.worldY + g.bee.screenY) {
+			if (this.collected == false && this.displayNow == true) {
+				g2.drawImage(image, screenX, screenY, width, height, null);
+			}
+		}
     }
+
+	public void switchVisibility() {
+		timer = new Timer(3000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (displayNow == true) {
+					displayNow = false;
+				} else {
+					displayNow = true;
+				}
+			}
+		});
+
+	}
 
     // public void showBonusReward() {
     //     try {
