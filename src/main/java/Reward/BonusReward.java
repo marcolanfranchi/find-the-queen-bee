0c3fc4a671d256dc4b6d29bf7f9663f3e6982a76
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2022-11-05 14:25:43 -0700

    feat: Now Bonus rewards spawn at random times

--- src/main/java/Reward/BonusReward.java
+++ src/main/java/Reward/BonusReward.java
@@ -1,7 +1,9 @@
 package reward;
+
 import main.GamePanel;
 
 import java.io.IOException;
+import java.util.Random;
 
 import javax.swing.Timer;
 
@@ -17,33 +19,38 @@ import java.awt.*;
 // Randomly appears!!
 public class BonusReward extends Reward {
 
-    public static int bonusRewardVal = 50;
-    public boolean displayNow;
-    public int drawBuffer = 10;
-	public Timer timer;
+	public static int bonusRewardVal = 50;
+	public boolean displayNow;
+	private Timer timer;
 
-    public BonusReward(GamePanel gp) {
-        super(gp);
-        this.value = bonusRewardVal;
-        this.displayNow = false;
-        this.value = bonusRewardVal;
+	private int minimal = 1000;
+	private int max = 4000;
+	private int randomTimeOutput;
+	Random randomTime;
+
+	public BonusReward(GamePanel gp) {
+		super(gp);
+		this.value = bonusRewardVal;
+		this.displayNow = false;
+		this.value = bonusRewardVal;
+
+		randomTime = new Random();
+		randomTimeOutput = (minimal + randomTime.nextInt(max - minimal + 1));
 		switchVisibility();
 		timer.start();
 
-        try {
-            this.image = ImageIO.read(getClass().getResource("../ui/images/HoneyPot.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			this.image = ImageIO.read(getClass().getResource("../ui/images/HoneyPot.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public void draw(Graphics2D g2, GamePanel g) {
+		int screenX = worldX - g.bee.worldX + g.bee.screenX;
+		int screenY = worldY - g.bee.worldY + g.bee.screenY;
 
-
-    public void draw(Graphics2D g2, GamePanel g) {
-        int screenX = worldX - g.bee.worldX + g.bee.screenX;
-        int screenY = worldY - g.bee.worldY + g.bee.screenY;
-
-        if (worldX + g.tileSize > g.bee.worldX - g.bee.screenX &&
+		if (worldX + g.tileSize > g.bee.worldX - g.bee.screenX &&
 				worldX - g.tileSize < g.bee.worldX + g.bee.screenX &&
 				worldY + g.tileSize > g.bee.worldY - g.bee.screenY &&
 				worldY - g.tileSize < g.bee.worldY + g.bee.screenY) {
@@ -51,10 +58,13 @@ public class BonusReward extends Reward {
 				g2.drawImage(image, screenX, screenY, width, height, null);
 			}
 		}
-    }
+	}
 
 	public void switchVisibility() {
-		timer = new Timer(3000, new ActionListener() {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((10000 - 2000) + 1) + 20000;
+
+		timer = new Timer(randomNum, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (displayNow == true) {
@@ -62,13 +72,21 @@ public class BonusReward extends Reward {
 				} else {
 					displayNow = true;
 				}
+
+				updateTimer();
 			}
 		});
 
 	}
 
+	private void updateTimer() {
+		timer.stop();
+		randomTimeOutput = (minimal + randomTime.nextInt(max - minimal + 1));
+		timer.setDelay(randomTimeOutput);
+		timer.restart();
+	}
 
-    public void collectReward(Bee bee) {
-        bee.beeScore += this.value;
-    }
+	public void collectReward(Bee bee) {
+		bee.beeScore += this.value;
+	}
 }
