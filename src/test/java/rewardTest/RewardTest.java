d60703abddc3ddc931a0481773e7c1cd86983ea1
sdallalz <sdallalz@sfu.ca>
2022-11-21 20:27:46 -0800

    reward generator revised

--- src/test/java/rewardTest/RewardTest.java
+++ src/test/java/rewardTest/RewardTest.java
@@ -5,11 +5,11 @@ import main.KeyHandler;
 
 import org.junit.jupiter.api.Test;
 
-import Reward.Reward;
-import Reward.RewardGenerator;
+import reward.Reward;
+import reward.RewardGenerator;
 
 import org.junit.jupiter.api.BeforeEach;
-
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +29,57 @@ class RewardGeneratorTest {
     void generateAllRewards()
     {
         assertEquals(12,RewardGenerator.getRewardsList().size(), "Failed to create all rewards by the generator");
+    }
+
+    @Test
+    void generateRegularReward()
+    {
+        // Test generateRegularReward
+    
+        RewardGenerator rewardGenerator = new RewardGenerator(map);
+        assertNotNull(rewardGenerator.getRewardsList(), "Failed to create a regular reward by the generator");
+    }
+
+    @Test
+    void generateBonusReward()
+    {
+        // Test generateBonusReward
+        RewardGenerator rewardGenerator = new RewardGenerator(map);
+        assertNotNull(rewardGenerator.getRewardsList(), "Failed to create a bonus reward by the generator");
+    }
+
+    @Test
+    void rewardOnRoad()
+    {
+    
+        RewardGenerator rewardGenerator = new RewardGenerator(map);
+        ArrayList<Reward> rewardsList = rewardGenerator.getRewardsList();
+        boolean check = true;
+        for(Reward e:rewardsList)
+        {
+            //if(!reward.gameObjects[(int) e.getMap().getY()][(int) e.getMap().getX()].getClass().getSimpleName().equals("Road"))
+            {
+                check = false;
+            }
+        }
+        assertTrue(check, "Some rewards have not been generated on the road");
+    }
+
+    @Test
+    void rewardInTheSamePosition()
+    {
+
+        RewardGenerator rewardGenerator = new RewardGenerator(map);
+        ArrayList<Reward> rewardsList = rewardGenerator.getRewardsList();
+        boolean check = true;
+        for(Reward e:rewardsList)
+        {
+            for(Reward e2:rewardsList)
+                if (e != e2 & e.getMap().equals(e2.getMap())) {
+                    check = false;
+                    break;
+                }
+        }
+        assertTrue(check, "Some rewards have been generated in the same location");
     }
 }
